7150cd1765a0ef6a03bc38f24055bebeb8551337
dlwogns1100-cell <dlwogns1100@gmail.com>
2026-06-17 10:28:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("ATREIA WORKS BOT START");
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`${client.user.tag} 로그인 완료`);
+});
+
+client.login(process.env.TOKEN);
