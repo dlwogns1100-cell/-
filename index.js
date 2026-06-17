7e0c8345616d68c17342ae3aae1d798b7bf27845
dlwogns1100-cell <dlwogns1100@gmail.com>
2026-06-17 10:43:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-require("dotenv").config();
-
-console.log("TOKEN EXISTS:", !!process.env.TOKEN);
-console.log("TOKEN LENGTH:", process.env.TOKEN?.length);
-console.log("TOKEN START:", process.env.TOKEN?.substring(0, 10));
-
+console.log(process.env.TEST);
 process.exit(0);
