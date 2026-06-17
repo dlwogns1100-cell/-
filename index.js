f1cf71de51e69ae83cd820ec1ef0a417e0fea821
dlwogns1100-cell <dlwogns1100@gmail.com>
2026-06-17 11:16:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 require("dotenv").config();
 
-console.log("ENV KEYS:");
-console.log(Object.keys(process.env));
+const {
+  Client,
+  GatewayIntentBits,
+  Events
+} = require("discord.js");
 
-console.log(
-  "TOKEN 존재:",
-  process.env.TOKEN
-);
 
-process.exit();
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`✅ ${c.user.tag} 온라인`);
+});
+
+
+client.on(Events.MessageCreate, async (message) => {
+
+  if (message.author.bot) return;
+
+
+  if (!message.content.includes("/")) return;
+
+
+  const text = message.content;
+
+
+  await message.delete().catch(() => {});
+
+
+  await message.channel.send(
+`${message.member?.displayName || message.author.username}님 작성
+
+${text}`
+  );
+
+});
+
+
+client.login(process.env.TOKEN)
+.catch(err => {
+  console.log("❌ 로그인 실패");
+  console.log(err.message);
+});
