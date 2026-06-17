da8138d3db0178eb1aa21d5f0357fda92228ff2f
dlwogns1100-cell <dlwogns1100@gmail.com>
2026-06-17 11:07:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,7 @@
 require("dotenv").config();
 
-const {
-  Client,
-  GatewayIntentBits,
-  Events
-} = require("discord.js");
+console.log("길이:", process.env.TOKEN?.length);
+console.log("앞:", process.env.TOKEN?.slice(0,5));
+console.log("뒤:", process.env.TOKEN?.slice(-5));
 
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
-});
-
-
-// 봇 접속
-client.once(Events.ClientReady, (client) => {
-  console.log(`✅ ${client.user.tag} 온라인`);
-});
-
-
-// 메시지 정리 기능
-client.on(Events.MessageCreate, async (message) => {
-
-  // 봇 메시지 무시
-  if (message.author.bot) return;
-
-
-  // "/" 포함된 신청글만 처리
-  if (!message.content.includes("/")) return;
-
-
-  const text = message.content;
-
-
-  // 원본 삭제
-  await message.delete().catch(() => {});
-
-
-  // 다시 작성
-  await message.channel.send(
-`${message.member?.displayName || message.author.username}님 작성
-
-${text}`
-  );
-
-});
-
-
-client.login(process.env.TOKEN)
-  .catch((err) => {
-    console.log("❌ 로그인 실패");
-    console.log(err.message);
-  });
+process.exit();
